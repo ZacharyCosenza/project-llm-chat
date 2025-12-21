e971adcf186789b1827c4181cab3f100b2218358
ZacharyCosenza <cosenzazachary@gmail.com>
2025-12-21 12:03:30 -0500

    no one look

--- startup.sh
+++ startup.sh
@@ -6,7 +6,5 @@ apt install python3.11 python3.11-venv python3.11-distutils -y
 python3.11 -m venv .venv
 source .venv/bin/activate
 
-export WANDB_API_KEY='5e8326722f21375acd3f670aec952cff0c9c47a4'
-
 python -m pip install --upgrade pip
 pip install numpy pandas matplotlib scikit-learn torch huggingface transformers hf_transfer lightning wandb
