fb7cf0cdbced2e7718ea6b814c2ec3e3eba96252
ZacharyCosenza <cosenzazachary@gmail.com>
2025-12-21 11:47:40 -0500

    new startup commands

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
-sudo apt update
-sudo apt install python3.11 python3.11-venv python3.11-distutils -y
+apt update
+apt install vim
+apt install tmux
+apt install python3.11 python3.11-venv python3.11-distutils -y
 
 python3.11 -m venv .venv
 source .venv/bin/activate
